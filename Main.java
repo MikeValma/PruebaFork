2a932a480f88054fc758ed926ffa5c30f7fffc76
Mikel <mikelurroz@iesbalmasedabhi.com>
2026-01-05 18:02:45 +0100

    MAIN CON NOMBRES

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Mikel";
+        String apellido = "Urroz";
+
+        String saludo = "Hola " + nombre + " " + apellido + " ¿Que tal?";
+
+        System.out.println(saludo);
+    }
+    
+}
