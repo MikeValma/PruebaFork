7d38353548085b91faddff1e8875e152e77c51a6
Mikel <mikelurroz@iesbalmasedabhi.com>
2026-01-21 08:57:00 +0100

    Nombre laura

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String nombre = "JON";
+        String nombre = "Laura";
         String apellido = "Garcia";
 
         String saludo = "Hola " + nombre + " " + apellido + " ¿Que tal?";
@@ -9,6 +9,7 @@ public class Main {
     }
     
 }
+
 
 
 
